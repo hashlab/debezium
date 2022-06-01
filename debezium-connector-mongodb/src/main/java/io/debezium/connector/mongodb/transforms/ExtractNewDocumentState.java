08229552de3a908b3b8b26278eddad0ea8346cf9
Robinson Nakamura <robmnk@gmail.com>
2022-06-01 11:39:55 -0300

    Revert "Return document id on delete operation"
    This reverts commit 0ee6427f28a7350f02999dae54d7872ec4ff63e4.

--- debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/transforms/ExtractNewDocumentState.java
+++ debezium-connector-mongodb/src/main/java/io/debezium/connector/mongodb/transforms/ExtractNewDocumentState.java
@@ -259,7 +259,6 @@ public class ExtractNewDocumentState<R extends ConnectRecord<R>> implements Tran
 
         if (handleDeletes.equals(DeleteHandling.REWRITE)) {
             valueDocument.append(ExtractNewRecordStateConfigDefinition.DELETED_FIELD, new BsonBoolean(isDeletion));
-            valueDocument.append("id", keyDocument.get("id"));
         }
 
         return newRecord(record, keyDocument, valueDocument);
